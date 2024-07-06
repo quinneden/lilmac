29d94b1e99a384d98e3b724de4f96dc4b3d3f4dd
Gabriel Guerra <gfranklings@gmail.com>
2024-07-06 19:47:15 -0400

    chore: disable gtk theming

--- config.nix
+++ config.nix
@@ -21,7 +21,7 @@ rec {
       # wether or not i should enable gtk configurations
       # disable if per example you're gonna use gnome as main
       # desktop environment
-      gtk.enable = true;
+      gtk.enable = false;
 
       aetherShell = {
         enable = true;
